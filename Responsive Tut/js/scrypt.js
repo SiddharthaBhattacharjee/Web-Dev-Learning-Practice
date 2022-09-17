5719161f5136f58824bc0dc665eec967686a4d19
Siddhartha <siddharthabhatt2345@gmail.com>
2022-09-17 22:56:21 +0530

    RespTutCompleted

--- Responsive Tut/js/scrypt.js
+++ Responsive Tut/js/scrypt.js
@@ -8,3 +8,14 @@ function githubopen(){
 function twitteropen(){
     webopen("https://twitter.com/Siddhartha_2345");
 }
+
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+rightNav = document.querySelector('.rightNav');
+navlist = document.querySelector('.nav-list');
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class');
+    navlist.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+})
